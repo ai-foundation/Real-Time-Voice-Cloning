7cff8c021ff49af879b6b6064c6ac515e9852f79
Jonathan Chu <jonathan@aifoundation.com>
2019-09-10 18:08:36 -0700

    fix indentation

--- rnnoise.py
+++ rnnoise.py
@@ -31,35 +31,35 @@ class RNNoise(object):
     def destroy(self):
         lib.rnnoise_destroy(self.obj)
 
-    def read_wave(path):
-        """Reads a .wav file.
-        Takes the path, and returns (PCM audio data, sample rate).
-        """
-        with contextlib.closing(wave.open(path, 'rb')) as wf:
-            num_channels = wf.getnchannels()
-            assert num_channels == 1
-            sample_width = wf.getsampwidth()
-            assert sample_width == 2
-            sample_rate = wf.getframerate()
-            assert sample_rate in (8000, 16000, 32000, 48000)
-            pcm_data = wf.readframes(wf.getnframes())
-            return pcm_data, sample_rate
-      
-    def frame_generator(frame_duration_ms,
-                        audio,
-                        sample_rate):
-        """Generates audio frames from PCM audio data.
-        Takes the desired frame duration in milliseconds, the PCM data, and
-        the sample rate.
-        Yields Frames of the requested duration.
-        """
-        n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
-        offset = 0
-        timestamp = 0.0
-        duration = (float(n) / sample_rate) / 2.0
-        while offset + n < len(audio):
-            yield audio[offset:offset + n]
-            offset += n
+def read_wave(path):
+    """Reads a .wav file.
+    Takes the path, and returns (PCM audio data, sample rate).
+    """
+    with contextlib.closing(wave.open(path, 'rb')) as wf:
+        num_channels = wf.getnchannels()
+        assert num_channels == 1
+        sample_width = wf.getsampwidth()
+        assert sample_width == 2
+        sample_rate = wf.getframerate()
+        assert sample_rate in (8000, 16000, 32000, 48000)
+        pcm_data = wf.readframes(wf.getnframes())
+        return pcm_data, sample_rate
+    
+def frame_generator(frame_duration_ms,
+                    audio,
+                    sample_rate):
+    """Generates audio frames from PCM audio data.
+    Takes the desired frame duration in milliseconds, the PCM data, and
+    the sample rate.
+    Yields Frames of the requested duration.
+    """
+    n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
+    offset = 0
+    timestamp = 0.0
+    duration = (float(n) / sample_rate) / 2.0
+    while offset + n < len(audio):
+        yield audio[offset:offset + n]
+        offset += n
     
 denoiser = RNNoise()
 
