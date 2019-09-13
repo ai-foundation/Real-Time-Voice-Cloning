663e6dba11209e5c63f88c67f8ff2da0e9229ea1
Jonathan Chu <jonathan@aifoundation.com>
2019-09-13 15:22:21 -0700

    upload audio transcript and use for force alignment

--- server.py
+++ server.py
@@ -67,13 +67,13 @@ def denoise_output(rnnoise_script_location, tmp_dir, audio_fpath):
     return denoised_wav_location
 
 # TODO: option to include custom transcript instead of only demo script
-def create_new_speaker_embedding(speaker, filename, useDemoScript):
-    if useDemoScript:
-        subprocess.call(["python", train_new_speaker_script_location, "-c", "conf.json", "--enc_model_fpath=/home/jonathan/rt-voice-cloning-models/encoder/saved_models/pretrained.pt",
-        "--speaker_name=" + speaker, "--audio_fpath=" + voice_clips_location + "/" + filename, "--from_api=True", "--transcript_fpath=/home/jonathan/Real-Time-Voice-Cloning/demo_script.txt"])
-    else:
+def create_new_speaker_embedding(speaker, filename, transcriptFileLocation):
+    if not transcriptFileLocation:
         subprocess.call(["python", train_new_speaker_script_location, "-c", "conf.json", "--enc_model_fpath=/home/jonathan/rt-voice-cloning-models/encoder/saved_models/pretrained.pt",
         "--speaker_name=" + speaker, "--audio_fpath=" + voice_clips_location + "/" + filename, "--from_api=True" ])
+    else:
+        subprocess.call(["python", train_new_speaker_script_location, "-c", "conf.json", "--enc_model_fpath=/home/jonathan/rt-voice-cloning-models/encoder/saved_models/pretrained.pt",
+        "--speaker_name=" + speaker, "--audio_fpath=" + voice_clips_location + "/" + filename, "--from_api=True", "--transcript_fpath=" + transcriptFileLocation])
 
 #####################################################################################
 #  Define server and other global variables
@@ -108,15 +108,26 @@ def train():
     print(speaker)
     filename = request.args.get('filename')
     print(filename)
-    useDemoScript = request.args.get('demo-script')
-    if (useDemoScript == 'True'):
-        useDemoScript = True
+    # useDemoScript = request.args.get('demo-script')
+    # if (useDemoScript == 'True'):
+    #     useDemoScript = True
+
+    transcriptFile = request.files['transcript']
+    transcriptFileLocation = ''
+    if transcriptFile:
+        transcriptFileName = speaker + '-' + filename + '-transcript.txt'
+        transcriptFileLocation = voice_clips_location + '/' + transcriptFileName
+        transcriptFile.save(transcriptFileLocation)
     
     audioFile = request.files['audioFile']
     # # TODO: ALLOWED FILES METHOD
     if audioFile:
         audioFile.save(voice_clips_location + '/' + filename)
-        create_new_speaker_embedding(speaker, filename, useDemoScript)
+        create_new_speaker_embedding(speaker, filename, transcriptFileLocation)
+
+        if transcriptFile:
+            os.remove(transcriptFileLocation)
+
         return jsonify({"status": "ok"})
 
     abort(400, "Invalid audio file")
