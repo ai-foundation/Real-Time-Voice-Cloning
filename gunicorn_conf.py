7f73b5bb55c9124b4130357734140bfadc37d4cf
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-30 02:49:05 +0000

    tune gunicorn conf for this instance

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -8,8 +8,9 @@ import os
 host = os.getenv("HOST", "0.0.0.0")
 port = os.getenv("PORT", "6009")
 bind = f"{host}:{port}"
+# suggested number of workers + threads: (2*CPU)+1
 workers = 1
 worker_class = "gthread"
-threads = 2
+threads = 1
 
 
